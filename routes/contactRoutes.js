13da5d2bf70f4603e3c712d774fff4ffea2fc95f
kengserch <saengkaew_p2@silpakorn.edu>
2024-01-15 14:50:11 +0700

    Express-MongoDB-RestApi

---
+++ routes/contactRoutes.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const router = express.Router();
+const {
+    getContacts,
+    createContact,
+    getContact,
+    updateContact,
+    deleteContact
+} = require("../controllers/contactController")
+
+
+
+router.route("/").get(getContacts).post(createContact);
+
+router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
+
+
+
+
+// router.route("/").get(getContacts);
+
+// router.route("/").post(createContact);
+
+// router.route("/:id").get(getContact);
+
+// router.route("/:id").put(updateContact);
+
+// router.route("/:id").delete(deleteContact);
+
+
+
+
+
+
+
+// router.route("/").get((req,res) => {
+//     res.status(200).json({ message : "Get all Contacts" });
+// });
+
+// router.route("/").post((req,res) => {
+//     res.status(200).json({ message : "Created Contact"});
+// });
+
+// router.route("/:id").get((req,res) => {
+//     res.status(200).json({ message : `Get contact for ${req.params.id}` });
+// });
+
+// router.route("/:id").put((req,res) => {
+//     res.status(200).json({ message : `Update contact for ${req.params.id}` });
+// });
+
+// router.route("/:id").delete((req,res) => {
+//     res.status(200).json({ message : `Delete contact for ${req.params.id}`}) 
+// });
+
+module.exports = router;
